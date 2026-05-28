6e89ac0cff2f92ac17442a4fb5a781c729ff4ca4
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-05-28 17:59:22 +0100

    Added flask sqlalchemy
    - Added flask_sqlalchemy in requiremnts.txt
    - Set up sqlalchemy in __init__.py
    - Updated docker-compose, removed version as not needed

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# database object
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # To add more pages make sure we import and register them
+    # db and app connection
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///urls.db"
+    db.init_app(app)
+    
+
+    with app.app_context():
+        db.create_all()
+
     from .routes import main
     app.register_blueprint(main)
 
