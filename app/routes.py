e57ccd924676e458b29b6435caf6659a9468ccd1
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-05-28 18:41:39 +0100

    Updated routes, Added templates
    - Added functionality to routes, fetching url and generating a short code to store in database
    - Added index and result templates to display form and result

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
 # Where we put our HTTP routes
-from flask import Blueprint
+from flask import Blueprint, request, redirect, render_template
+from .models import Url
+from .utilities import generate_code
+from app import db
 
+# allows us to split into multiple route files
 main = Blueprint("main", __name__)
 
-@main.route("/")
+
+@main.route("/", methods=["GET"])
 def home():
-    return "Test"
+    return render_template("index.html")
+
+# generates a shortened code saves it to the database and returns short URL and result page
+@main.route("/create", methods=["POST"])
+def create():
+    long_url = request.form.get("url")
+
+    code = generate_code()
+    new = Url(short_code=code, long_url=long_url)
+    db.session.add(new)
+    db.session.commit()
+
+    short_url = f"http://localhost:5000/{code}"
+    return render_template("result.html", short_url=short_url)
+
+
+
+
+# Matches shortened code and looks it in the database, redirect user to long url
+@main.route("/<code>")
+def redirect_url(code):
+    url = Url.query.filter_by(short_code=code).first_or_404()
+    return redirect(url.long_url)
+
