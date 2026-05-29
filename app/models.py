ccc20411496ba743e085550cdd5e683510be7250
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-05-29 16:17:42 +0100

    Added URL history and updated database model
    - Added page_title column in model.py
    - Used mechanize module to fetch page title and insert into database in routes.py
    - Fetched all URLs in DB to display as table in index.html

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     short_code = db.Column(db.String(10), unique=True, nullable=False)
     long_url = db.Column(db.String(2048), nullable=False)
+    page_title = db.Column(db.String(2048), nullable = False)
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from flask import Blueprint, request, redirect, render_template
 from .models import Url
 from .utilities import generate_code
 from app import db
+from mechanize import Browser
+
+# Create Browser instance
+br = Browser()
+
 
 # allows us to split into multiple route files
 main = Blueprint("main", __name__)
@@ -10,15 +15,23 @@ main = Blueprint("main", __name__)
 
 @main.route("/", methods=["GET"])
 def home():
-    return render_template("index.html")
+    # Fetch previous URLs from DB
+    # If no results become false to display msg front-end
+    history = Url.query.all() or False
+    return render_template("index.html", history = history)
 
 # generates a shortened code saves it to the database and returns short URL and result page
 @main.route("/create", methods=["POST"])
 def create():
     long_url = request.form.get("url")
 
+    # Get the title of the page
+    br.open(long_url)
+    page_title = br.title()
+    br.close()
+
     code = generate_code()
-    new = Url(short_code=code, long_url=long_url)
+    new = Url(short_code=code, long_url=long_url, page_title=page_title)
     db.session.add(new)
     db.session.commit()
 
